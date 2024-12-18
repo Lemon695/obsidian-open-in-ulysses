8a339e4c7b4a502f79c6051a9389d6527d387ea1
Lemon695 <Lemon4560@outlook.com>
2024-12-19 02:51:20 +0800

    feat(open-in-ulysses):

--- main.ts
+++ main.ts
@@ -1,85 +1,29 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {exec} from 'child_process';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface UlyssesPluginSettings {
+	ulyssesPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: UlyssesPluginSettings = {
+	ulyssesPath: '/Applications/UlyssesMac.app'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class UlyssesPlugin extends Plugin {
+	settings: UlyssesPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// 添加命令到Obsidian命令面板
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'open-in-ulysses',
+			name: 'Open current file in Ulysses',
+			callback: () => this.openInUlysses()
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		// 添加设置选项卡
+		this.addSettingTab(new UlyssesSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -89,45 +33,59 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	openInUlysses() {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			new Notice('没有打开任何文件');
+			return;
+		}
+
+		const filePath = activeFile.path;
+		const resourcePath = this.app.vault.adapter.getResourcePath(filePath);
+
+		// 尝试从 resourcePath 中提取实际文件路径
+		const match = resourcePath.match(/app:\/\/[^/]+(.+)\?/);
+		const fullPath = match ? decodeURIComponent(match[1]) : null;
+
+		if (!fullPath) {
+			new Notice('无法获取文件路径');
+			return;
+		}
+
+		console.log(`fullPath=${fullPath}`);
+
+		// 使用MacOS的open命令打开文件
+		exec(`open -a "${this.settings.ulyssesPath}" "${fullPath}"`, (error) => {
+			if (error) {
+				new Notice(`打开文件失败: ${error.message}`);
+			} else {
+				new Notice('已在Ulysses中打开文件');
+			}
+		});
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class UlyssesSettingTab extends PluginSettingTab {
+	plugin: UlyssesPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: UlyssesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Ulysses 应用路径')
+			.setDesc('设置Ulysses应用的完整路径')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('/Applications/UlyssesMac.app')
+				.setValue(this.plugin.settings.ulyssesPath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.ulyssesPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
